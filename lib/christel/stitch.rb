3e1e74dbfef662ea806f161223031eed4de2916e
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2015-12-18 07:48:20 +0100

    add 'edges' to Stitch (node) class.

--- lib/christel/stitch.rb
+++ lib/christel/stitch.rb
@@ -1,8 +1,15 @@
 module Christel
   class Stitch
     attr_accessor :type
-    def initialize(type)
-      @type = type
+    attr_accessor :predecessor
+    attr_accessor :destination
+    attr_accessor :stitched_by
+
+    def initialize(type, predecessor: nil, destination: nil)
+      @type        = type
+      @predecessor = predecessor
+      @destination = destination
+      @stitched_by = []
     end
   end
 end
--- spec/stich_spec.rb
+++ spec/stich_spec.rb
@@ -4,4 +4,36 @@ describe Christel::Stitch do
   it 'initializes with a type' do
     expect(Christel::Stitch.new(:ch)).not_to be nil
   end
+
+  describe '#type' do
+    it 'stores a type' do
+      expect(Christel::Stitch.new(:ch).type).to eql :ch
+    end
+  end
+
+  describe '#predecessor' do
+    it 'defaults to no predecessor' do
+      expect(Christel::Stitch.new(:st).predecessor).to be nil
+    end
+
+    it 'can be initialized with predecessor' do
+      predecessor = Christel::Stitch.new(:ch)
+      expect(Christel::Stitch.new(:st,
+               predecessor: predecessor).predecessor).to eql (predecessor)
+    end
+  end
+
+  describe '#destination' do
+    it 'stitches *into* other stitch' do
+      destination = Christel::Stitch.new(:ch)
+      expect(Christel::Stitch.new(
+         :st, destination: destination).destination).to eql destination
+    end
+  end
+
+  describe '#stitched_by' do
+    it 'stores list of stitches that have this stitch as destination' do
+      expect(Christel::Stitch.new(:st).stitched_by).to eql []
+    end
+  end
 end
