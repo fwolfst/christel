e69f63512e8403ea1c1a3fed9b8ae0a11f841456
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2015-12-18 08:11:58 +0100

    add braindead cpl grammar, polyglot magic.

--- lib/christel.rb
+++ lib/christel.rb
@@ -1,3 +1,10 @@
 require 'christel/version'
 require 'christel/stitch'
 require 'christel/pattern'
+
+require 'polyglot'
+require 'treetop'
+
+# Now, the grammar can be loaded (polyglot magic).
+# Note that also a Parser class is instantiated.
+require 'cpl/cpl_grammar'
