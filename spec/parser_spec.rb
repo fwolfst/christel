b94c3be5ff11a1356c2d82611c81cc23cc6b9284
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2015-12-21 20:46:30 +0100

    improve parser spec, tolerance towards input

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -9,8 +9,19 @@ describe CPLGrammarParser do
 
   describe 'creates Patterns from input' do
     it 'parses a dead simple example' do
-      pattern = @parser.parse("ch")
+      pattern = @parser.parse("ch").make_pattern
       expect(pattern).to_not be nil
+      expect(pattern.stitches.count).to be 1
+    end
+
+    it 'parses <num stitch> examples' do
+      pattern = @parser.parse("6 ch").make_pattern
+      expect(pattern.stitches.count).to be 6
+    end
+
+    it 'parses accepts comments' do
+      pattern = @parser.parse("# Comment\n6ch\n#Comment 2").make_pattern
+      expect(pattern.stitches.count).to be 6
     end
   end
 end
