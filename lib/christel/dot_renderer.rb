2ebda7d5070a2f7d1e50843ac66139560e58458b
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2015-12-21 20:30:46 +0100

    add edges, label to pattern graph/render

--- lib/christel/dot_renderer.rb
+++ lib/christel/dot_renderer.rb
@@ -3,17 +3,23 @@ require 'ruby-graphviz'
 module Christel
   class DotRenderer
     def render(pattern, filename)
-      graph = GraphViz.new("G", "type" => "digraph")
+      graph = GraphViz.new("G", "type" => "digraph", "label" => pattern.name)
+      # Look up Christel::Stitch -> Graphiz::Node
+      stitch_map = {}
       pattern.stitches.each_with_index do |stitch, stitch_index|
         node = graph.add_nodes("#{stitch.type} #{stitch_index}",
                            color:     'red',
                            fillcolor: 'white',
                            style:     'filled')
         node.label = stitch.type
+        stitch_map[stitch] = node
         # for predecessor
-        # add_edges(node, .., weight, len, color)
+        if predecessor = stitch_map[stitch.predecessor]
+          # weight len color
+          graph.add_edges(node, predecessor)
+        end
         # for destination
-        # add_edges(node, .., weight, len, color)
+        # graph.add_edges(node, .., weight, len, color)
       end
       graph.output(dot: "#{filename}.dot")
       graph.output(use: 'neato', png: "#{filename}.png")
