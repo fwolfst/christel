4ed436a6c0bf64152f5973d1e9869579a24370d5
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2015-12-29 18:00:42 +0100

    add ',' to pattern grammar.

---
+++ lib/cpl/instruction_node.rb
@@ -0,0 +1,12 @@
+module CPLGrammar
+  class InstructionNode < Treetop::Runtime::SyntaxNode
+    def operate_on pattern
+      # StitchCreationNodes
+
+      elements.each do |element|
+        pattern.add_stitch element.create_stitch(pattern) if element.respond_to?(:create_stitch)
+        element.operate_on(pattern) if element.respond_to?(:operate_on)
+      end
+    end
+  end
+end
